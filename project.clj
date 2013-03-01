c8e4926c4077d1db667c9696891e5d21de515ede
Soren Macbeth <soren@dopeness.org>
2013-03-01 13:14:16 -0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject deoptim "0.1.0-SNAPSHOT"
+  :description "Differential Evolution Optimizer"
+  :url "http://github.com/sorenmacbeth/deoptim"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [bigml/sampling "2.1.0"]])
---
+++ test/deoptim/core_test.clj
@@ -0,0 +1,7 @@
+(ns deoptim.core-test
+  (:use clojure.test
+        deoptim.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
