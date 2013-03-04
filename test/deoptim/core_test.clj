591d3debfa4e3ff45324717702410f6f2816eb61
homer <homer.strong@gmail.com>
2013-03-04 14:50:28 -0800

    add unscientific benchmark-test-example

--- test/deoptim/core_test.clj
+++ test/deoptim/core_test.clj
@@ -3,5 +3,14 @@
         deoptim.core))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Sphere is well optimized."
+    (defn sphere [^doubles xs]
+      (let [^doubles squares (amap ^doubles xs idx ret
+                                   (Math/pow (aget ^doubles xs idx) 2))]
+        (areduce ^doubles squares idx ret (double 0)
+                 (+ ret (aget ^doubles squares idx)))))
+    (time 
+     (optimize sphere 0.2 0.1 100 10 -100 100 150))
+    ;; initial trial times (ms): 5896, 5845, 5898, 5910, 6009
+    ;; after rewriting crossover (ms): 2354, 2363, 2437, 2392, 2546    
+    ))
