5662aa252cfbb1918553846d2e432c8aa463b430
Soren Macbeth <soren@dopeness.org>
2013-03-29 09:28:07 -0700

    bump to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :warn-on-reflection true
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.commons/commons-math3 "3.1.1"]])
